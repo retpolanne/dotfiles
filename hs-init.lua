21dcf3b28cc7832f4eca75149b57b811e784e6dd
Anne Isabelle "Anya" Macedo <annie@retpolanne.com>
2026-05-05 18:26:14 -0300

    Run hs on system sleep
    Signed-off-by: Anne Isabelle "Anya" Macedo <annie@retpolanne.com>

--- hs-init.lua
+++ hs-init.lua
@@ -1,5 +1,6 @@
 watcher = hs.caffeinate.watcher.new(function(event)
-        if event == hs.caffeinate.watcher.screensDidLock then
+        if event == hs.caffeinate.watcher.screensDidLock
+        or event == hs.caffeinate.watcher.systemWillSleep then
            print("screen locked")
            local output, status, type, rc = hs.execute("/opt/homebrew/bin/blueutil --disconnect '18-3f-70-dd-1f-ad'")
            print(rc)
